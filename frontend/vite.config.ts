b4813c2771679c12c7d4535f471803a13d95ca5c
Flaaim <flaaim@list.ru>
2025-12-11 21:56:56 +1000

    Added frontend node dev server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,8 +7,9 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    hmr: true,
     watch: {
-      usePolling: true  // Для Docker
+      usePolling: true
     }
   }
 })
