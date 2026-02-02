ebe5f49239319d9ee6a53419ace479278a32385f
Flaaim <flaaim@list.ru>
2026-02-03 09:55:22 +1000

    added Status component

---
+++ frontend/src/components/Status/Status.tsx
@@ -0,0 +1,31 @@
+import {JSX, ReactNode} from "react";
+import styles from './Status.module.css'
+import cn from 'classnames'
+import {Roboto_Mono} from "next/font/google";
+
+const robotoMono = Roboto_Mono({
+  variable: "--font-roboto-mono",
+  subsets: ["cyrillic"]
+})
+
+export interface PendingStatusProps {
+  children: ReactNode
+  appearance: 'pending' | 'success' | 'failed' | 'error' | 'loading'
+}
+export const Status = ({children, className, appearance}: PendingStatusProps): JSX.Element => {
+  return (<>
+      <div
+
+        className={ cn(styles.status, className, robotoMono.variable, {
+        [styles.success]: appearance === 'success',
+        [styles.failed]: appearance === 'failed' || appearance === 'error',
+        [styles.pending]: appearance === 'pending',
+        [styles.loading]: appearance === 'loading',
+      }
+      )
+      }>
+        {children}</div>
+      <p>Вернуться на <a href='/frontend/public'>главную.</a></p>
+  </>
+  )
+}
