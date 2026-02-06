ddaa7230a129c41a605ae66576a8ed959831e8b1
Flaaim <flaaim@list.ru>
2026-02-06 14:34:21 +1000

    changed styles

--- frontend/src/components/ProductInfo/ProductInfo.tsx
+++ frontend/src/components/ProductInfo/ProductInfo.tsx
@@ -2,13 +2,24 @@
 
 import {JSX, useEffect, useState} from "react";
 import {ProductInfoProps} from "@/components/ProductInfo/ProductInfo.props";
+import styles from './ProductInfo.module.css'
+import {DefItem, Deflisttag, DownloadButton, Spantag} from "@/components";
+import {Status} from "@/components/Status/Status";
+import {Roboto_Mono} from "next/font/google";
+import cn from 'classnames'
 
 export interface ProductInfoData {
   name: string
   price: number,
   format: string
-  count: number,
+  quantity: number,
 }
+
+const robotoMono = Roboto_Mono({
+  variable: "--font-roboto-mono",
+  subsets: ["cyrillic"]
+})
+
 
 export const ProductInfo = ({productId}: ProductInfoProps): JSX.Element => {
   const [error, setError] = useState<string | null>(null)
@@ -20,9 +31,9 @@ export const ProductInfo = ({productId}: ProductInfoProps): JSX.Element => {
       setLoading(true)
       setError(null)
 
-      const productInfo = await getProductInfo(productId)
-      if(productInfo){
-        setProductInfoData(productInfo)
+      const productInfoData = await getProductInfo(productId)
+      if(productInfoData){
+        setProductInfoData(productInfoData)
       }
 
       setLoading(false)
@@ -41,7 +52,7 @@ export const ProductInfo = ({productId}: ProductInfoProps): JSX.Element => {
       const data = response.json();
 
       if(!response.ok){
-        const errorMessage = data.message || `Ошибка ${response.status}: ${response.statusText}`
+        const errorMessage = 'Ошибка получения данных' || `Ошибка ${response.status}: ${response.statusText}`
 
         switch (response.status){
           case 400 : setError(`Ошибка: ${errorMessage}`)
@@ -52,6 +63,7 @@ export const ProductInfo = ({productId}: ProductInfoProps): JSX.Element => {
         }
         return null
       }
+      return data
     } catch (err) {
       setError(err instanceof Error ? err.message : 'Ошибка соединения с сервером')
       return null
@@ -59,10 +71,21 @@ export const ProductInfo = ({productId}: ProductInfoProps): JSX.Element => {
   }
 
   if(loading){
-    return <div>Загрузка...</div>
+    return <Status appearance='loading'>Загрузка...</Status>
   }
-
-  return (<div  data={productId}>
-    {error}
+  if(error){
+    return <Status appearance='error'>{error}</Status>
+  }
+  return (<div className={cn(styles.productInfo, robotoMono.variable)} data={productId}>
+    <Deflisttag >
+      <DefItem term='Название' definition={ProductInfoData?.name} />
+      <DefItem term='Стоимость' definition={ProductInfoData?.price + ` рублей`} />
+      <DefItem term='Количество' definition={ProductInfoData?.quantity + ` файлов`} />
+      <DefItem term='Формат файлов' definition={ProductInfoData?.format} />
+    </Deflisttag>
+    <DownloadButton >
+      <Spantag size='s' > Скачать </Spantag> <br />
+      <Spantag appearance='bold' size='m'>RAR Архив</Spantag>
+    </DownloadButton>
   </div>)
 }
