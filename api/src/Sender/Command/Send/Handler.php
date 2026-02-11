f5386c917a5d46fc939583aa69f317f14aa1e170
Flaaim <flaaim@list.ru>
2026-02-11 16:48:13 +1000

    added SendHandler

--- api/src/Sender/Command/Send/Handler.php
+++ api/src/Sender/Command/Send/Handler.php
@@ -4,39 +4,33 @@ namespace App\Sender\Command\Send;
 
 use App\Product\Entity\File;
 use App\Sender\Entity\Recipient;
+use App\Sender\Service\Message\CreatorInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Mailer\Exception\TransportException;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
-use Twig\Environment;
 
 class Handler
 {
     public function __construct(
         private readonly MailerInterface $mailer,
-        private readonly Environment $twig,
-        private readonly LoggerInterface $logger
+        private readonly LoggerInterface $logger,
+        private readonly CreatorInterface $creator,
     )
     {
+
     }
     public function handle(Recipient $recipient): void
     {
-        $message = new \Symfony\Component\Mime\Email();
-        $message->subject($recipient->getSubject());
-        $message->to($recipient->getEmail()->getValue());
-        $message->html(
-            $this->twig->render('mail/template.html.twig')
-        );
-        /** @var File $attachment */
-        foreach ($recipient->getAttachments() as $attachment) {
-            $message->attachFromPath($attachment->getFullPath());
-        }
-
         try{
+            $message = $this->creator->create($recipient);
             $this->mailer->send($message);
         } catch (TransportExceptionInterface $e) {
-            $this->logger->error('Failed to send mail: ', ['error' => $e->getMessage()]);
-            throw new TransportException($e->getMessage());
+            $this->logger->error('Failed to send mail: ', [
+                'error' => $e->getMessage(),
+                'recipient' => $recipient->getEmail()->getValue(),
+            ]);
+            throw $e;
         }
 
 
--- api/src/Sender/Test/Unit/Command/Send/HandlerTest.php
+++ api/src/Sender/Test/Unit/Command/Send/HandlerTest.php
@@ -5,60 +5,73 @@ namespace App\Sender\Test\Unit\Command\Send;
 use App\Payment\Entity\Email;
 use App\Sender\Command\Send\Handler;
 use App\Sender\Entity\Recipient;
+use App\Sender\Service\Message\CreatorInterface;
+use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Mailer\Exception\TransportException;
 use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Address;
-use Twig\Environment;
-use Symfony\Component\Mime\Email as SymfonyEmail;
+use Symfony\Component\Mime\Email as SymfonyMessage;
+
+#[CoversClass(Handler::class)]
 class HandlerTest extends TestCase
 {
     public function testSuccessSend(): void
     {
-        $recipient = new Recipient(new Email('test@email.ru'), 'Тестовая отправка письма');
+        $recipient = new Recipient(new Email('test@email.ru'), 'Тест');
+        $symfonyMessage = $this->getSymfonyMessage($recipient);
 
         $handler = new Handler(
             $mailer = $this->createMock(MailerInterface::class),
-            $twig = $this->createMock(Environment::class),
             $this->createMock(LoggerInterface::class),
+            $creator = $this->createMock(CreatorInterface::class),
         );
 
-        $mailer->expects(self::once())->method('send')->with(
-            $this->equalTo((new SymfonyEmail())
-                ->subject($recipient->getSubject())
-                ->to($recipient->getEmail()->getValue())
-                ->html($twig->render('mail/template.html.twig'))
-            )
-        )->willReturnCallback(static function ($message) use ($recipient, $twig) {
-            /** @var SymfonyEmail $message */
-            self::assertEquals($recipient->getSubject(), $message->getSubject());
-            self::assertEquals([new Address($recipient->getEmail()->getValue())], $message->getTo());
-            self::assertEquals($twig->render('mail/template.html.twig'), $message->getHtmlBody());
-        });
+        $creator->expects($this->once())
+            ->method('create')->with($recipient)->willReturn($symfonyMessage);
+
+        $mailer->expects(self::once())->method('send')
+            ->with($symfonyMessage);
 
         $handler->handle($recipient);
     }
 
     public function testFailedSend(): void
     {
-        $recipient = new Recipient(new Email('test@email.ru'), 'Тестовая отправка письма');
+        $recipient = new Recipient(new Email('test@email.ru'), 'Тест');
+        $symfonyMessage = $this->getSymfonyMessage($recipient);
 
         $handler = new Handler(
             $mailer = $this->createMock(MailerInterface::class),
-            $twig = $this->createMock(Environment::class),
             $logger = $this->createMock(LoggerInterface::class),
+            $creator = $this->createMock(CreatorInterface::class),
         );
 
-        $mailer->expects(self::once())->method('send')->willThrowException(new TransportException($message ='TransportException'));
+        $creator->expects($this->once())
+            ->method('create')->with($recipient)->willReturn($symfonyMessage);
 
-        $logger->expects(self::once())->method('error')->with(
+        $mailer->expects(self::once())->method('send')
+            ->with($symfonyMessage)->willThrowException(new TransportException('Transport failed.'));
+
+        $logger->expects($this->once())->method('error')->with(
             $this->equalTo('Failed to send mail: '),
-            $this->equalTo(['error' => $message])
+            $this->equalTo([
+                'error' => 'Transport failed.',
+                'recipient' => 'test@email.ru',
+            ])
         );
 
         self::expectException(TransportException::class);
-
+        self::expectExceptionMessage('Transport failed.');
         $handler->handle($recipient);
+    }
+
+    private function getSymfonyMessage(Recipient $recipient): SymfonyMessage
+    {
+        $message = new SymfonyMessage();
+        $message->to($recipient->getEmail()->getValue());
+        $message->subject($recipient->getSubject());
+
+        return $message;
     }
 }
