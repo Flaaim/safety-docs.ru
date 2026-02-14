29d8fd96a080c8fde54ef6098f18e113c67e82db
Flaaim <flaaim@list.ru>
2026-02-15 09:46:36 +1000

    update sidebar

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,9 @@
 import {JSX} from "react";
 import {SidebarProps} from "./Sidebar.props";
-import {Htag, Ptag, Spantag, DefItem, DownloadButton, Deflisttag } from "@/components";
-import styles from './Sidebar.module.css'
+import {Htag, Ptag, Spantag } from "@/components";
 import classNames from "classnames";
 import {ProductInfo} from "@/components/ProductInfo/ProductInfo";
+import Link from "next/link";
 
 
 
@@ -11,13 +11,8 @@ import {ProductInfo} from "@/components/ProductInfo/ProductInfo";
 
 export const Sidebar = ({className, ...props}:SidebarProps): JSX.Element => {
   return (
-    <aside {...props} className={classNames(className, styles.sidebar)}>
-      <Htag tag='h1'>Охрана труда документы 2026г.</Htag>
-      <Ptag >Полный комплект локальных нормативных актов по охране труда</Ptag>
-      <Ptag >Блог охраны труда <br />
-        <Spantag ><a href="https://t.me/help_ot_news">https://t.me/help_ot_news</a>
-        </Spantag>
-      </Ptag>
+    <aside {...props} className={classNames(className)}>
+
       <Htag tag='h3'>Информация:</Htag>
       <ProductInfo
         productId='e2ff37fb-8690-46e5-82fa-75b5ceca8b61'
@@ -25,5 +20,9 @@ export const Sidebar = ({className, ...props}:SidebarProps): JSX.Element => {
         formatFiles='docx, excel'
         description=''>
       </ProductInfo>
+      <Ptag >Блог охраны труда <br />
+        <Spantag ><Link href="https://t.me/help_ot_news">https://t.me/help_ot_news</Link>
+        </Spantag>
+      </Ptag>
     </aside>)
 }
