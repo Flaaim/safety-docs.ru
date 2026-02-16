4d42d34747030fa62ba05762d156883fe2831a47
Flaaim <flaaim@list.ru>
2026-02-16 16:42:38 +1000

    added Price to Payment

---
+++ api/src/Payment/Entity/PriceType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Payment\Entity;
+
+use App\Shared\Domain\ValueObject\Currency;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\StringType;
+
+class PriceType extends StringType
+{
+    public const NAME = 'price';
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): mixed
+    {
+        return $value instanceof Price ? $value->getValue() : $value;
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform): ?Price
+    {
+        return !empty($value) ? new Price((float)$value, new Currency('RUB')) : null;
+    }
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+
+    public function getSQLDeclaration(array $column, AbstractPlatform $platform): string
+    {
+        $column['length'] = 36;
+        $column['fixed'] = true; // CHAR вместо VARCHAR
+
+        return $platform->getStringTypeDeclarationSQL($column);
+    }
+}
