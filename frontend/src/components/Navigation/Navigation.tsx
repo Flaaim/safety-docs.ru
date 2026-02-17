e95b4485851e241bf2f93b58903271bd6e43112d
Flaaim <flaaim@list.ru>
2026-02-17 15:09:36 +1000

    added props to navigation

--- frontend/src/components/Navigation/Navigation.tsx
+++ frontend/src/components/Navigation/Navigation.tsx
@@ -1,7 +1,8 @@
 import {JSX} from "react";
 import styles from './Navigation.module.css'
+import {NavigationProps} from "@/components/Navigation/Navigation.props";
 
-export const Navigation = ({children}): JSX.Element => {
+export const Navigation = ({children}: NavigationProps): JSX.Element => {
   return <div className={styles.navigation}>
     {children}
   </div>
