082018d450e07c76bab37e2a8e312e52ea861429
Flaaim <flaaim@list.ru>
2026-03-04 18:39:15 +1000

    added breadcrumbs

---
+++ frontend/src/components/Breadcrumb/Breadcrumbs.tsx
@@ -0,0 +1,40 @@
+import {Fragment, JSX} from "react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import {BreadcrumbsProps} from "@/components/Breadcrumb/Breadcrumbs.props";
+import { breadcrumbsConfig } from "@/configs/breadcrumbs.config";
+export default function Breadcrumbs ({ path }: BreadcrumbsProps ): JSX.Element  {
+  const items = breadcrumbsConfig[path] || [];
+
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/">Главная</BreadcrumbLink>
+        </BreadcrumbItem>
+
+        {items.map((item, index) => {
+          const isLast = index === items.length - 1;
+          return (
+            <Fragment key={item.link}>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                {isLast ? (
+                  <BreadcrumbPage>{item.title}</BreadcrumbPage>
+                ) : (
+                  <BreadcrumbLink href={item.link}>{item.title}</BreadcrumbLink>
+                )}
+              </BreadcrumbItem>
+            </Fragment>
+          );
+        })}
+      </BreadcrumbList>
+    </Breadcrumb>
+  )
+}
---
+++ frontend/src/configs/breadcrumbs.config.tsx
@@ -0,0 +1,35 @@
+
+export interface BreadcrumbItem {
+  title: string;
+  link: string;
+}
+
+export const breadcrumbsConfig: Record<string, BreadcrumbItem[]> = {
+  "/safety": [
+    { title: "Охрана труда", link: "/safety" }
+  ],
+  "/safety/service": [
+    { title: "Охрана труда", link: "/safety" },
+    { title: "Служба охраны труда", link: "/safety/service" }
+  ],
+  "/safety/suot": [
+    { title: "Охрана труда", link: "/safety" },
+    { title: "СУОТ", link: "/safety/suot" }
+  ],
+  "/safety/education": [
+    { title: "Охрана труда", link: "/safety" },
+    { title: "Обучение охраны труда", link: "/safety/education" }
+  ],
+  "/safety/medical": [
+    { title: "Охрана труда", link: "/safety" },
+    { title: "Медосмотры", link: "/safety/medical" }
+  ],
+  "/safety/firstaid": [
+    { title: "Охрана труда", link: "/safety" },
+    { title: "Аптечки", link: "/safety/firstaid" }
+  ],
+  "/safety/rules": [
+    { title: "Охрана труда", link: "/safety" },
+    { title: "Инструкции по охране труда", link: "/safety/rules" }
+  ]
+}
