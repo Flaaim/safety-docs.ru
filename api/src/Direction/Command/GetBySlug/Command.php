ef92955d857a1c84f8cd99e95bd3bf4dcc7f98ea
Flaaim <flaaim@list.ru>
2026-03-13 15:31:31 +1000

    added assert validation

--- api/src/Direction/Command/GetBySlug/Command.php
+++ api/src/Direction/Command/GetBySlug/Command.php
@@ -2,9 +2,11 @@
 
 namespace App\Direction\Command\GetBySlug;
 
+use Symfony\Component\Validator\Constraints as Assert;
 class Command
 {
     public function __construct(
+        #[Assert\NotBlank]
         public string $slug,
     ){
     }
