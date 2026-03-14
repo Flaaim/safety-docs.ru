1b331887a44f9764db3f48b1cf56e889d8f4d009
Flaaim <flaaim@list.ru>
2026-03-14 11:46:10 +1000

    added request getAll directions

---
+++ api/src/Http/Action/V1/Direction/GetAll/RequestAction.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Action\V1\Direction\GetAll;
+
+use App\Direction\Command\GetAll\Handler;
+use App\Http\JsonResponse;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+
+class RequestAction implements RequestHandlerInterface
+{
+
+    public function __construct(
+        private readonly Handler $handler
+    ){
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+
+        $response = $this->handler->handle();
+
+        return new JsonResponse($response);
+    }
+}
