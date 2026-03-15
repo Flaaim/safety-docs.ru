d6c8613ca31adbfde151fb67093c083a971b8c86
Flaaim <flaaim@list.ru>
2026-03-15 10:14:07 +1000

    added generateMetadata function and cached fetch

--- frontend/src/app/(site)/directions/[slug]/page.tsx
+++ frontend/src/app/(site)/directions/[slug]/page.tsx
@@ -5,6 +5,12 @@ import {Htag} from "@/components";
 import React from "react";
 import MarkdownRenderer from "@/components/MarkdownRenderer";
 import normalizeMarkdown  from "@/utils/normalizeMarkdown";
+import { cache } from 'react';
+
+
+const getCachedDirection = cache(async (slug: string) => {
+  return await getDirectionBySlug(slug);
+})
 
 export  async function generateStaticParams() {
   const data = await getAllDirections()
@@ -15,11 +21,26 @@ export  async function generateStaticParams() {
 }
 
 
+export async function generateMetadata({ params }: { params: Promise< {slug:string} >}) {
+  const { slug } = await params;
+  try{
+    const data = await getCachedDirection(slug);
+    return {
+      title: `${data.title} | Образцы документов`,
+      description: `Комплекты документов по направлению: ${data.title}.`,
+    };
+  }catch (error){
+    return {
+      title: "Направление не найдено",
+    };
+  }
+}
+
 export default async function DirectionPage({ params }: { params: Promise< {slug:string} > }) {
   const { slug } = await params;
 
   try{
-    const data = await getDirectionBySlug(slug);
+    const data = await getCachedDirection(slug);
 
       return (
         <>
