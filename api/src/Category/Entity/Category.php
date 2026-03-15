0efab9e26480a0769317ef5ee34273fe82e71027
Flaaim <flaaim@list.ru>
2026-03-15 14:51:06 +1000

    added mapping Category

--- api/src/Category/Entity/Category.php
+++ api/src/Category/Entity/Category.php
@@ -2,14 +2,25 @@
 
 namespace App\Category\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'categories')]
 class Category
 {
     public function __construct(
+        #[ORM\Id]
+        #[ORM\Column(type: 'category_id', unique: true)]
         private CategoryId $categoryId,
+        #[ORM\Column(type: 'string', length: 150)]
         private string $title,
+        #[ORM\Column(type: 'string', length: 255)]
         private string $description,
+        #[ORM\Column(type: 'text')]
         private string $text,
+        #[ORM\Column(type: 'category_slug', length: 35)]
         private Slug $slug,
+        #[ORM\Column(type: 'string', unique: true)]
         private string $directionId
     ){
     }
