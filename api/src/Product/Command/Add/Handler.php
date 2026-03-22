7a6f35b37aa5ce8fd7e8c90297da37ab461bbb23
Flaaim <flaaim@list.ru>
2026-03-22 13:51:40 +1000

    added Product Add Handler

---
+++ api/src/Product/Command/Add/Handler.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Product\Command\Add;
+
+use App\Flusher;
+use App\Product\Entity\Amount;
+use App\Product\Entity\File;
+use App\Product\Entity\Product;
+use App\Product\Entity\ProductId;
+use App\Product\Entity\ProductRepository;
+use App\Product\Entity\Slug;
+use App\Shared\Domain\ValueObject\Currency;
+use Ramsey\Uuid\Uuid;
+
+class Handler
+{
+    public function __construct(
+        private readonly ProductRepository $products,
+        private readonly Flusher $flusher,
+    ){
+    }
+
+    public function handle(Command $command): void
+    {
+        $slug = new Slug($command->slug);
+        $product = $this->products->findBySlug($slug);
+        if($product) {
+            throw new \DomainException("Product with slug " .$command->slug. " already exists.");
+        }
+        $product = new Product(
+            ProductId::generate(),
+            $command->name,
+            new Amount($command->amount, new Currency('RUB')),
+            new File($command->path),
+            $command->cipher,
+            $slug,
+            new \DateTimeImmutable($command->updatedAt)
+        );
+        $this->products->add($product);
+
+        $this->flusher->flush();
+
+    }
+}
---
+++ api/src/Product/Test/Command/Add/HandlerTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Product\Test\Command\Add;
+
+use App\Flusher;
+use App\Product\Command\Add\Command;
+use App\Product\Command\Add\Handler;
+use App\Product\Entity\Product;
+use App\Product\Entity\ProductId;
+use App\Product\Entity\ProductRepository;
+use App\Product\Entity\Slug;
+use App\Product\Test\ProductBuilder;
+use PHPUnit\Framework\TestCase;
+
+class HandlerTest extends TestCase
+{
+    private ProductRepository $products;
+    private Flusher $flusher;
+
+    private Handler $handler;
+    public function setUp(): void
+    {
+        $this->products = $this->createMock(ProductRepository::class);
+        $this->flusher = $this->createMock(Flusher::class);
+        $this->handler = new Handler($this->products, $this->flusher);
+    }
+
+    public function testExists(): void
+    {
+        $command = $this->createCommand();
+
+        $slug = new Slug($command->slug);
+        $product = (new ProductBuilder())->build();
+        $this->products->expects(self::once())->method('findBySlug')
+            ->with($this->equalTo($slug))
+            ->willReturn($product);
+
+        $this->flusher->expects(self::never())->method('flush');
+
+        self::expectException(\DomainException::class);
+        self::expectExceptionMessage('Product with slug '.$command->slug.' already exists');
+
+        $this->handler->handle($command);
+    }
+
+    public function testSuccess(): void
+    {
+        $command = $this->createCommand();
+
+        $slug = new Slug($command->slug);
+
+        $this->products->expects(self::once())->method('findBySlug')
+            ->with($this->equalTo($slug))
+            ->willReturn(null);
+
+        $this->products->expects(self::once())->method('add')
+            ->with(self::callback(function(Product $product) {
+                self::assertEquals('Обучение по охране труда - комплект документов', $product->getName());
+                self::assertEquals('edu300.1', $product->getCipher());
+                self::assertEquals(550.00, $product->getAmount()->getValue());
+                self::assertEquals('safety/education', $product->getFile()->getValue());
+                self::assertEquals('education', $product->getSlug()->getValue());
+                return true;
+            }));
+
+        $this->handler->handle($command);
+    }
+
+    private function createCommand(): Command
+    {
+        return new Command(
+            'Обучение по охране труда - комплект документов',
+            'edu300.1',
+            550.00,
+            'safety/education',
+            'education',
+            (new \DateTimeImmutable())->format('d.m.Y')
+        );
+    }
+}
