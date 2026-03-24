7cae19a69a19a3c2cf60b5ed2de0925f9116e102
Flaaim <flaaim@list.ru>
2026-03-25 08:02:55 +1000

    added UpdateFile handler middleware

---
+++ api/src/Http/Middleware/UploadFileHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\UploadedFileInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class UploadFileHandler implements MiddlewareInterface
+{
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $uploadedFiles = $request->getUploadedFiles();
+
+        $fileEntry = $uploadedFiles['file'] ?? null;
+
+        $file = is_array($fileEntry) ? ($fileEntry[0] ?? null) : $fileEntry;
+
+        if (!$file instanceof UploadedFileInterface || $file->getError() !== UPLOAD_ERR_OK) {
+            throw new \DomainException('File is required.');
+        }
+
+        $request = $request->withAttribute('target_file', $file);
+
+        return $handler->handle($request);
+    }
+}
---
+++ api/src/Http/Test/Unit/Middleware/UploadFileHandlerTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Test\Unit\Middleware;
+
+use App\Http\Middleware\UploadFileHandler;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\UploadedFileInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Psr7\Factory\ServerRequestFactory;
+
+class UploadFileHandlerTest extends TestCase
+{
+
+    public function testFileMissing(): void
+    {
+        $uploadFileHandler = new UploadFileHandler();
+
+        $request = (new ServerRequestFactory())->createServerRequest('POST', '/test')->withUploadedFiles([]);
+
+        $handler = $this->createMock(RequestHandlerInterface::class);
+
+        self::expectException(\DomainException::class);
+        self::expectExceptionMessage('File is required.');
+        $uploadFileHandler->process($request, $handler);
+    }
+
+    public function testHandle(): void
+    {
+        $uploadFileHandler = new UploadFileHandler();
+        $uploadedFile = $this->createMock(UploadedFileInterface::class);
+
+        $request = (new ServerRequestFactory())->createServerRequest('POST', '/test')->withUploadedFiles(['file' => [$uploadedFile]]);
+
+        $handler = $this->createMock(RequestHandlerInterface::class);
+
+        $handler->expects($this->once())->method('handle')
+            ->with(self::callback(static function ($request) {
+                self::assertInstanceOf(UploadedFileInterface::class, $request->getAttribute('target_file'));
+                return true;
+            }));
+
+        $uploadFileHandler->process($request, $handler);
+    }
+}
