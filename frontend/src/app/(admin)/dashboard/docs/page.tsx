db79a1933c46864342d7870aaf1efcc996cdd29c
Flaaim <flaaim@list.ru>
2026-03-31 15:39:13 +1000

    added docs page

---
+++ frontend/src/app/(admin)/dashboard/docs/page.tsx
@@ -0,0 +1,50 @@
+import {cookies} from "next/headers";
+import {getAllProducts} from "@api/product";
+import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
+import {ProductDTO} from "@/interfaces/product.interface";
+import AddProductDialog from "@/components/Admin/Dashboard/Docs/add-product-dialog";
+
+
+export default async function ProductPage() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("admin_token")?.value;
+
+  const data = await getAllProducts(token);
+
+  return (
+    <div className="space-y-6">
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl font-bold">Подборки документов</h1>
+        <AddProductDialog></AddProductDialog>
+      </div>
+      <div className="rounded-md border bg-white">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Название</TableHead>
+              <TableHead>Шифр</TableHead>
+              <TableHead>Цена</TableHead>
+              <TableHead>Обновлен</TableHead>
+              <TableHead>Файл</TableHead>
+              <TableHead className="text-right">Действия</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data.products.map((prod: ProductDTO) => (
+              <TableRow key={prod.id}>
+                <TableCell className="font-medium">{prod.name}</TableCell>
+                <TableCell className="font-medium">{prod.cipher}</TableCell>
+                <TableCell className="font-medium">{prod.price}</TableCell>
+                <TableCell className="font-medium">{prod.updatedAt}</TableCell>
+                <TableCell className="font-medium">{prod.file}</TableCell>
+                <TableCell className="text-right">
+
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  )
+}
