beb46cb25a75f529849ebd550b7de55ffd627252
Flaaim <flaaim@list.ru>
2026-03-31 11:24:05 +1000

    added getAllProducts method

--- frontend/api/product.tsx
+++ frontend/api/product.tsx
@@ -1,6 +1,6 @@
 
 import {API} from "@/app/api";
-import {ProductInfoData} from "@/interfaces/product.interface";
+import {ProductCollection, ProductDTO, ProductInfoData} from "@/interfaces/product.interface";
 
 
 export async function getProductBySlug(slug: string): Promise<ProductInfoData> {
@@ -9,6 +9,26 @@ export async function getProductBySlug(slug: string): Promise<ProductInfoData> {
     headers: {'Content-Type': 'application/json'},
   })
   if(!response.ok){
+    console.error(`HTTP error! status: ${response.status} status text: ${response.statusText}`)
+    throw new Error(`Ошибка получения данных`);
+  }
+  return response.json();
+}
+export async function getAllProducts(token: string | undefined): Promise<ProductCollection>{
+  const headers: HeadersInit = {
+    'Content-Type': 'application/json',
+  }
+
+  if(token){
+    headers['Authorization'] = `Bearer ${token}`;
+  }
+
+  const response = await fetch(API.product.getAll(), {
+    method: "GET",
+    headers: headers,
+    cache: token ? 'no-store' : 'force-cache'
+  })
+  if (!response.ok) {
     console.error(`HTTP error! status: ${response.status} status text: ${response.statusText}`)
     throw new Error(`Ошибка получения данных`);
   }
