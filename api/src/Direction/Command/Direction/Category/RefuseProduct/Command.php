d41716649f57c7ab56b1f85a5f9bd36f456c1699
Flaaim <flaaim@list.ru>
2026-04-01 18:28:49 +1000

    added refuseProduct handler

---
+++ api/src/Direction/Command/Direction/Category/RefuseProduct/Command.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Direction\Command\Direction\Category\RefuseProduct;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Command
+{
+    public function __construct(
+        #[Assert\NotBlank]
+        #[Assert\Uuid]
+        public string $productId,
+        #[Assert\NotBlank]
+        #[Assert\Uuid]
+        public string $categoryId,
+    )
+    {}
+}
