0b38ddbe0d8cf8eee3366cdf9fa3f84be4efcce3
Flaaim <flaaim@list.ru>
2026-04-03 15:01:03 +1000

    added handle method

--- api/src/Product/Command/Update/UpdateProductHandlerInterface.php
+++ api/src/Product/Command/Update/UpdateProductHandlerInterface.php
@@ -4,5 +4,6 @@ namespace App\Product\Command\Update;
 
 interface UpdateProductHandlerInterface
 {
+    public function handle(Command $command): void;
     public function getType(string $type): bool;
 }
