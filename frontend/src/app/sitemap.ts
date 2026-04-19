f8e00a97c728616a5a72a35c60bda68508df7207
Flaaim <flaaim@list.ru>
2026-04-19 13:46:12 +1000

    updated sitemap xml

--- frontend/src/app/sitemap.ts
+++ frontend/src/app/sitemap.ts
@@ -7,10 +7,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://safety-docs.ru';
   return [
     { url: `${baseUrl}/`, lastModified: new Date() },
-    { url: `${baseUrl}/safety`, lastModified: new Date() },
-    { url: `${baseUrl}/safety/education`, lastModified: new Date() },
-    { url: `${baseUrl}/safety/service`, lastModified: new Date() },
-    { url: `${baseUrl}/safety/suot`, lastModified: new Date() },
+    { url: `${baseUrl}/docs/safety`, lastModified: new Date() },
+    { url: `${baseUrl}/docs/energy`, lastModified: new Date() },
+    { url: `${baseUrl}/docs/safety/syot-complect-documentov`, lastModified: new Date() },
+    { url: `${baseUrl}/docs/safety/safety-education-complect-documentov`, lastModified: new Date() },
+    { url: `${baseUrl}/docs/safety/sluzhba-ohrany-truda-komplekt-dokumentov`, lastModified: new Date() },
+    { url: `${baseUrl}/docs/safety/iot-and-pravila-po-ohrane-truda-complect`, lastModified: new Date() },
     { url: `${baseUrl}/success`, lastModified: new Date() },
   ];
 }
