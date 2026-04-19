d64843631d6dbe1a50ed207c34a6a7eecb41221f
Flaaim <flaaim@list.ru>
2026-04-20 09:22:30 +1000

    update sitemap

--- frontend/src/app/sitemap.ts
+++ frontend/src/app/sitemap.ts
@@ -13,6 +13,9 @@ export default function sitemap(): MetadataRoute.Sitemap {
     { url: `${baseUrl}/docs/safety/safety-education-complect-documentov`, lastModified: new Date() },
     { url: `${baseUrl}/docs/safety/sluzhba-ohrany-truda-komplekt-dokumentov`, lastModified: new Date() },
     { url: `${baseUrl}/docs/safety/iot-and-pravila-po-ohrane-truda-complect`, lastModified: new Date() },
+    { url: `${baseUrl}/docs/energy/15-prikazov-po-electrobezopastnosti`, lastModified: new Date() },
+    { url: `${baseUrl}/docs/energy/13-zhurnalov-otvetstvennogo-za-elektrohozyajstvo`, lastModified: new Date() },
+    { url: `${baseUrl}/docs/industrial/boiler-room-dogovor-arendy-complect`, lastModified: new Date() },
     { url: `${baseUrl}/success`, lastModified: new Date() },
   ];
 }
