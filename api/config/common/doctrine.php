8d30d44fb4dae79e9de6415bc6f5ccd100117283
Flaaim <flaaim@list.ru>
2026-05-02 21:56:20 +1000

    fix: validate schema error

--- api/config/common/doctrine.php
+++ api/config/common/doctrine.php
@@ -23,6 +23,10 @@ return [
             null
         );
 
+        $config->setSchemaAssetsFilter(function ($assetName){
+            return !str_starts_with($assetName, 'doctrine_migration_versions');
+        });
+
         $config->setNamingStrategy(new UnderscoreNamingStrategy());
 
         foreach ($settings['types'] as $name => $class) {
