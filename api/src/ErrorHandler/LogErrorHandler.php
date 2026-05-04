50232f526a2062193a3c515f891dbe990afcd767
Flaaim <flaaim@list.ru>
2026-05-04 15:28:05 +1000

    refactored LogErrorHandler.php

--- api/src/ErrorHandler/LogErrorHandler.php
+++ api/src/ErrorHandler/LogErrorHandler.php
@@ -2,25 +2,10 @@
 
 namespace App\ErrorHandler;
 
-use Monolog\Logger;
-use Psr\Http\Message\ResponseFactoryInterface;
-use Psr\Log\LoggerInterface;
 use Slim\Handlers\ErrorHandler;
-use Slim\Interfaces\CallableResolverInterface;
 
 class LogErrorHandler extends ErrorHandler
 {
-    protected LoggerInterface $logger;
-    public function __construct(
-        CallableResolverInterface $callableResolver,
-        ResponseFactoryInterface $responseFactory,
-        LoggerInterface $logger
-    )
-    {
-        parent::__construct($callableResolver, $responseFactory);
-        $this->logger = $logger;
-    }
-
     protected function writeToErrorLog(): void
     {
         $this->logger->error($this->exception->getMessage(), [
