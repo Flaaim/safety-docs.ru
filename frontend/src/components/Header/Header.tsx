c9cf522a66b252c70010c12f7619a040acdf00f3
Flaaim <flaaim@list.ru>
2026-05-10 23:06:33 +1000

    refactored navigation link in Header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,16 +1,23 @@
 import {HeaderProps} from "@/components/Header/Header.props";
-import {JSX} from "react";
+import {cache, JSX} from "react";
 import cn from 'classnames';
 import styles from './Header.module.css';
 import Link from "next/link";
+import {getAllDirections} from "@api/direction";
 
-export const Header = ({className}:HeaderProps): JSX.Element => {
-  return <header className={cn(className, styles.header)}>
+const getCachedDirections = cache(async () => {
+  return await getAllDirections();
+});
+
+export const Header = async ({className}: HeaderProps): Promise<JSX.Element> => {
+  const data = await getCachedDirections();
+  return (<header className={cn(className, styles.header)}>
     <nav>
-        <Link href="/" className={cn(className, styles.link)}>Главная</Link>
-        <Link href="/docs/safety" className={cn(className, styles.link)}>Охрана труда</Link>
-      <Link href="/docs/energy" className={cn(className, styles.link)}>Энергобезопасность</Link>
-      <Link href="/docs/industrial" className={cn(className, styles.link)}>Промбезопасность</Link>
+      <Link href="/" className={cn(className, styles.link)}>Главная</Link>
+    {data.directions.map((direction) => {
+      return ( <Link key={direction.id} href={'/docs/' + direction.slug} className={cn(className, styles.link)}>{direction.title}</Link>);
+    })}
     </nav>
-  </header>;
+    </header>
+    );
 };
