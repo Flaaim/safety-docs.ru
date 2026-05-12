4a5d5e843af7bacc731814eb42af14b2d8b0a7b0
Flaaim <flaaim@list.ru>
2026-05-13 09:44:53 +1000

    reordering middleware

--- api/config/middleware.php
+++ api/config/middleware.php
@@ -3,15 +3,18 @@
 declare(strict_types=1);
 
 use App\Http\Middleware;
+use App\Http\Middleware\RouteMiddleware;
 use Slim\App;
 use Slim\Middleware\ErrorMiddleware;
 
 return static function (App $app): void {
-    $app->add(Middleware\DomainExceptionHandler::class);
-    $app->add(Middleware\ValidationExceptionHandler::class);
 
     $app->add(Middleware\ClearInputHandler::class);
     $app->addBodyParsingMiddleware();
-
+    $app->add(RouteMiddleware::class);
+    $app->addRoutingMiddleware();
+    $app->add(Middleware\ValidationExceptionHandler::class);
+    $app->add(Middleware\DomainExceptionHandler::class);
     $app->add(ErrorMiddleware::class);
+
 };
