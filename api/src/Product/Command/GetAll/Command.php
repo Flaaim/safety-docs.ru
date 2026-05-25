e4eaa47e66f091e04ea7d980ee8bcf7c86209f63
Flaaim <flaaim@list.ru>
2026-05-26 06:59:57 +1000

    changed perPage

--- api/src/Product/Command/GetAll/Command.php
+++ api/src/Product/Command/GetAll/Command.php
@@ -6,7 +6,7 @@ class Command
 {
     public function __construct(
         public readonly int $page = 1,
-        public readonly int $perPage = 20,
+        public readonly int $perPage = 40,
     ) {
     }
 }
