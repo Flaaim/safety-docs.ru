48d58b4e26ae3b731d31d8774af3f5539bfe8dd1
Flaaim <flaaim@list.ru>
2026-05-29 15:57:33 +1000

    reconfigured SendTelegramHandler in DI

--- api/config/common/notifier.php
+++ api/config/common/notifier.php
@@ -2,16 +2,16 @@
 
 declare(strict_types=1);
 
-use App\Shared\Domain\Service\Notification\TelegramNotifier;
+use App\Shared\Domain\Service\Notification\MessageHandler\SendTelegramHandler;
 use GuzzleHttp\Client;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
 
 return [
-    TelegramNotifier::class => function (ContainerInterface $container) {
+    SendTelegramHandler::class => function (ContainerInterface $container) {
         $config = $container->get('config')['telegramBot'];
         $client = new Client(['base_uri' => 'https://api.telegram.org/']);
         $logger = $container->get(LoggerInterface::class);
-        return new TelegramNotifier($client, $logger, $config['token'], $config['chatId']);
+        return new SendTelegramHandler($client, $logger, $config['token'], $config['chatId']);
     },
 ];
