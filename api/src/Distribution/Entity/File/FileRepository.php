7bb73d287c0e94d3b78a498e8402d451e0103105
Flaaim <flaaim@list.ru>
2026-06-07 13:11:24 +1000

    added FileRepository.php

---
+++ api/src/Distribution/Entity/File/FileRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Distribution\Entity\File;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+
+final class FileRepository
+{
+    private EntityRepository $repo;
+    public function __construct(
+        private readonly EntityManagerInterface $em
+    )
+    {
+        $repo = $em->getRepository(File::class);
+        $this->repo = $repo;
+    }
+    public function add(File $file): void
+    {
+        $this->em->persist($file);
+    }
+}
