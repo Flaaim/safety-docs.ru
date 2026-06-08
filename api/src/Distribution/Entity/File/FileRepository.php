c6382768f7e31145c99bec039d8a25faeb73a133
Flaaim <flaaim@list.ru>
2026-06-09 08:12:06 +1000

    added findPaginated

--- api/src/Distribution/Entity/File/FileRepository.php
+++ api/src/Distribution/Entity/File/FileRepository.php
@@ -6,6 +6,7 @@ namespace App\Distribution\Entity\File;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
 final class FileRepository
 {
@@ -31,5 +32,21 @@ final class FileRepository
     public function findByName(string $name): ?File
     {
         return $this->repo->findOneBy(['name' => $name]);
+    }
+    public function findPaginated(int $page, int $perPage, string $sortBy = 'date', string $sortDir = 'DESC'): array
+    {
+        $sortDir = strtoupper($sortDir) === 'ASC' ? 'ASC' : 'DESC';
+
+        $qb = $this->repo->createQueryBuilder('p')
+            ->orderBy('p.' . $sortBy, $sortDir)
+            ->setFirstResult(($page - 1) * $perPage)
+            ->setMaxResults($perPage);
+
+        $paginator = new Paginator($qb);
+
+        return [
+            'items' => iterator_to_array($paginator),
+            'total' => count($paginator),
+        ];
     }
 }
