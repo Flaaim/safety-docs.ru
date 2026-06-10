8563605fb0d018e996f67ed19a0859943634c088
Flaaim <flaaim@list.ru>
2026-06-11 09:14:26 +1000

    added NewsletterRepository

---
+++ api/src/Distribution/Entity/Newsletter/NewsletterRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Distribution\Entity\Newsletter;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+
+final class NewsletterRepository
+{
+    private EntityRepository $repo;
+    public function __construct(
+        private readonly EntityManagerInterface $em
+    ) {
+        $repo = $em->getRepository(Newsletter::class);
+        $this->repo = $repo;
+    }
+
+    public function add(Newsletter $newsletter): void
+    {
+        $this->em->persist($newsletter);
+    }
+
+}
