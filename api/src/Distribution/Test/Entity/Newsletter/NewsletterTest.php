3ba175113582d0ce7679476a9da309e6da368ed6
Flaaim <flaaim@list.ru>
2026-06-11 14:29:15 +1000

    added ProjectId in entity test

--- api/src/Distribution/Test/Entity/Newsletter/NewsletterTest.php
+++ api/src/Distribution/Test/Entity/Newsletter/NewsletterTest.php
@@ -7,17 +7,24 @@ namespace App\Distribution\Test\Entity\Newsletter;
 use App\Distribution\Entity\Newsletter\Newsletter;
 use App\Distribution\Entity\Newsletter\NewsletterId;
 use App\Distribution\Entity\Newsletter\Status;
+use App\Distribution\Entity\Project\Project;
+use App\Distribution\Entity\Project\ProjectId;
 use PHPUnit\Framework\TestCase;
 
 final class NewsletterTest extends TestCase
 {
     public function testCreate(): void
     {
+        $project = new Project(
+            ProjectId::generate(),
+            'Блог охраны труда'
+        );
         $newsletter = new Newsletter(
             $id = NewsletterId::generate(),
             $subject = 'Обновления на сайте',
             $templateId = 'd255c7a2-64e7-4cb0-b419-69a2340e61b5',
-            $status = Status::created()
+            $status = Status::created(),
+            $project->getId()
         );
 
         self::assertEquals($id, $newsletter->getId());
