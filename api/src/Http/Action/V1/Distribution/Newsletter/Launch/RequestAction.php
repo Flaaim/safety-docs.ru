e706716c2c452b0e547093cddd36ab88cb63a199
Flaaim <flaaim@list.ru>
2026-06-12 22:41:53 +1000

    phpcs fix

--- api/src/Http/Action/V1/Distribution/Newsletter/Launch/RequestAction.php
+++ api/src/Http/Action/V1/Distribution/Newsletter/Launch/RequestAction.php
@@ -15,9 +15,10 @@ use Psr\Http\Server\RequestHandlerInterface;
 final class RequestAction implements RequestHandlerInterface
 {
     public function __construct(
-      private readonly Handler $handler,
-      private readonly Validator $validator
-    ) {}
+        private readonly Handler $handler,
+        private readonly Validator $validator
+    ) {
+    }
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         $body = $request->getParsedBody() ?? [];
