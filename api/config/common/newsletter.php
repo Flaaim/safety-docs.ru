b2bc548109dc0f2054238e2abfa94b844caee45f
Flaaim <flaaim@list.ru>
2026-06-12 21:28:50 +1000

    added newsletter config

---
+++ api/config/common/newsletter.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+
+use App\Distribution\Service\NewLetterLauncher;
+use App\Distribution\Service\NewsletterLauncherInterface;
+use GuzzleHttp\Client;
+use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
+
+return [
+    NewsletterLauncherInterface::class => function (ContainerInterface $container): NewsletterLauncherInterface {
+        $client = new Client([
+            'base_uri' => 'https://goapi.unisender.ru/ru/transactional/api/v1/',
+        ]);
+
+        $logger = $container->get(LoggerInterface::class);
+        $apiKey = $container->get('config')['uniSender']['apiKey'];
+
+        return new NewLetterLauncher($client, $apiKey, $logger);
+    },
+    'config' => [
+        'uniSender' => [
+            'apiKey' => getenv('UNI_SENDER_API'),
+        ]
+    ]
+];
