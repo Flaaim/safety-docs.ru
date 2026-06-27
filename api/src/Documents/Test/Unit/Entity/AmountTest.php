cb6195fb9fac33feb0c27c7c3c54277d4929eb0d
Flaaim <flaaim@list.ru>
2026-06-28 08:07:25 +1000

    added Amount to Document module

---
+++ api/src/Documents/Test/Unit/Entity/AmountTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Documents\Test\Unit\Entity;
+
+use App\Product\Entity\Amount;
+use App\Shared\Domain\ValueObject\Currency;
+use PHPUnit\Framework\TestCase;
+
+class AmountTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $price = new Amount(150.00, new Currency('RUB'));
+        $this->assertEquals(150.00, $price->getValue());
+    }
+    public function testInvalid(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        new Amount(0.00, new Currency('RUB'));
+    }
+    public function testRound(): void
+    {
+        $price = new Amount(150.00000, new Currency('RUB'));
+        $this->assertEquals(150.00, $price->getValue());
+    }
+
+    public function testEquals(): void
+    {
+        $price = new Amount(150.00, new Currency('RUB'));
+        $newPrice = new Amount(150.00, new Currency('RUB'));
+        $this->assertTrue($price->equals($newPrice));
+    }
+}
