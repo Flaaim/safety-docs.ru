8e0cce49c4e9e53da77d76280dfe776be309632e
Flaaim <flaaim@list.ru>
2026-07-02 11:52:16 +1000

    removed images paths

--- api/config/common/system.php
+++ api/config/common/system.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use App\Product\Service\File\FileUploaderInterface;
 use App\Shared\Domain\ValueObject\FileSystem\DistributionSystemPath;
 use App\Shared\Domain\ValueObject\FileSystem\FileSystemPath;
 use App\Shared\Domain\ValueObject\FileSystem\ImageSystemPath;
@@ -14,17 +13,11 @@ return [
         'login' => getenv('AUTH_LOGIN'),
         'password' => getenv('AUTH_PASSWORD'),
         'template_paths' => __DIR__ . '/../../public/templates',
-        'images_paths' => __DIR__ . '/../../public/images',
         'distribution_import_files' => __DIR__ . '/../../public/distributions/import',
     ],
     FileSystemPath::class => function (ContainerInterface $container) {
         return new FileSystemPath(
             $container->get('config')['template_paths'],
-        );
-    },
-    ImageSystemPath::class => function (ContainerInterface $container) {
-        return new FileSystemPath(
-            $container->get('config')['images_paths'],
         );
     },
     DistributionSystemPath::class => function (ContainerInterface $container) {
