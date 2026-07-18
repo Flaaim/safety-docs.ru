21a0bdb720b900f1e8e71bf297ad0f7aa9b52136
Flaaim <Flaaim@list.ru>
2026-07-18 15:16:37 +1000

    updated Sentry config

--- api/public/index.php
+++ api/public/index.php
@@ -4,11 +4,22 @@ declare(strict_types=1);
 
 use Psr\Container\ContainerInterface;
 use Slim\App;
+use Slim\Exception\HttpBadRequestException;
+use Slim\Exception\HttpForbiddenException;
+use Slim\Exception\HttpMethodNotAllowedException;
+use Slim\Exception\HttpNotFoundException;
+
 
 require __DIR__ . '/../vendor/autoload.php';
 
 \Sentry\init([
-    'dsn' => 'https://9198a832ad0371b66d65eb292a025f5e@o4511418890452992.ingest.de.sentry.io/4511523894001744',
+    'dsn' => 'https://0f744193cb82499f8710e89dc555c57c@o4510080200998912.ingest.de.sentry.io/4511753314566224',
+    'ignore_exceptions' => [
+        HttpNotFoundException::class,
+        HttpMethodNotAllowedException::class,
+        HttpForbiddenException::class,
+        HttpBadRequestException::class,
+    ]
 ]);
 
 /** @var  ContainerInterface $container */
