46bbab739aa12da2d108495e409497b51df270dd
Flaaim <Flaaim@list.ru>
2026-07-20 09:11:35 +1000

    added sentry to worker

--- api/bin/worker.php
+++ api/bin/worker.php
@@ -2,6 +2,10 @@
 
 declare(strict_types=1);
 
+use Slim\Exception\HttpBadRequestException;
+use Slim\Exception\HttpForbiddenException;
+use Slim\Exception\HttpMethodNotAllowedException;
+use Slim\Exception\HttpNotFoundException;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Messenger\Transport\TransportInterface;
@@ -9,7 +13,13 @@ use Symfony\Component\Messenger\Worker;
 
 require __DIR__ . '/../vendor/autoload.php';
 \Sentry\init([
-    'dsn' => 'https://9198a832ad0371b66d65eb292a025f5e@o4511418890452992.ingest.de.sentry.io/4511523894001744',
+    'dsn' => 'https://0f744193cb82499f8710e89dc555c57c@o4510080200998912.ingest.de.sentry.io/4511753314566224',
+    'ignore_exceptions' => [
+        HttpNotFoundException::class,
+        HttpMethodNotAllowedException::class,
+        HttpForbiddenException::class,
+        HttpBadRequestException::class,
+    ]
 ]);
 $container = require __DIR__ . '/../config/container.php';
 
