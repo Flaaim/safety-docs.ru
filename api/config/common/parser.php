e254cb0dec73a194541fd2507d0105fd6c63db95
Flaaim <Flaaim@list.ru>
2026-07-24 14:48:50 +1000

    added env AI_REWRITER_API_KEY

--- api/config/common/parser.php
+++ api/config/common/parser.php
@@ -2,12 +2,29 @@
 
 declare(strict_types=1);
 
+use App\Parser\Service\DocumentAiRewriter;
 use GuzzleHttp\Client;
 use GuzzleHttp\ClientInterface;
+use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
 
 return [
     ClientInterface::class => function () {
         return new Client([
-            'base_uri' => 'https://1otruda.ru']);
-    }
+            'base_uri' => 'https://1otruda.ru'
+        ]);
+    },
+    DocumentAiRewriter::class => function (ContainerInterface $container) {
+        $apiKey = $container->get('config')['AiRewriter'];
+        return new DocumentAiRewriter(
+            $container->get(ClientInterface::class),
+            $container->get(LoggerInterface::class),
+            $apiKey['apiKey']
+        );
+    },
+    'config' => [
+        'AiRewriter' => [
+            'apiKey' => getenv('AI_REWRITER_API_KEY'),
+        ]
+    ]
 ];
