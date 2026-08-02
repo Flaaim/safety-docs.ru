9653a6f534b23c3bb80b3f95dc6467a9956733d3
Flaaim <Flaaim@list.ru>
2026-08-02 12:09:10 +1000

    removed force-dymamic

--- frontend/src/app/(site)/directions/[directionSlug]/[categorySlug]/[templateSlug]/page.tsx
+++ frontend/src/app/(site)/directions/[directionSlug]/[categorySlug]/[templateSlug]/page.tsx
@@ -10,8 +10,6 @@ import TemplatePreview from "@/components/Template/TemplatePreview";
 import RelatedTemplates from "@/components/Template/RelatedTemplates";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
-export const dynamic = "force-dynamic";
-
 const getCachedDirection = cache(async (slug: string) => {
   return await getDirectionBySlug(slug);
 });
